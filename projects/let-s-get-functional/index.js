fbe27d894e13a25de6bb993a2221dce2c4eeaabf
Ubuntu <ubuntu@ip-172-31-83-66.ec2.internal>
2020-04-20 04:17:52 +0000

    pushing lets-get-functional with pseudo

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -21,104 +21,150 @@ var _ = require("lodown-gduple");
  *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
  */
 
-var maleCount = function(array) {
-    let filterOutput = _.filter(array, function(customerObj) {
-        //filter out all but the male customers
-        return customerObj.gender === 'male'; 
+var maleCount = function(array) {  // function takes array of customer objects
+    let filterOutput = _.filter(array, function(customerObj) { // filter out ...
+        return customerObj.gender === 'male'; // but male customers (rtns array)
     });
-    return filterOutput.length; 
+    return filterOutput.length; //return length of array of customers who passed
 };
 
 
-// var femaleCount = function(array){
-//     // input array of cust obj
-//     // output number
-//     return _.reduce(array, function(prev, current){
-//         // when to increment the seed 
-//         // if our cust obj has a gender of female
-//       if (current.gender === 'female'){
-//           prev += 1;
-//       }
-//         // when to not increment our seed
-//         // when our cust obj isnt a female
-//     return prev;
-//     }, 0);
-// };
 
-var femaleCount = function(array){
-    // input array of cust obj
-    // output number
-    return _.reduce(array, function(prev){
-        // when to increment the seed 
-        // if our cust obj has a gender of female
-       if (prev.gender === 'female'){
-           prev += 1;
+var femaleCount = function(array){ // function takes array of customer objects
+    return _.reduce(array, function(prev, current) { //reduce to count females
+       if (current.gender === 'female') {   //if current elem's gender is female:
+           prev += 1;                       //add one to the count 
        }
-        // when to not increment our seed
-        // when our cust obj isnt a female
-    return prev; 
-    }, array[0]);
+    return prev;                            // return prev, which is the count
+    }, 0);                                  // seed of 0 to start counting from 
 };
 
 
 
-var oldestCustomer = function(array) {
-    var ages = _.pluck(array, 'age'); 
-    var oldest = 0; 
-    for (let i = 0; i < ages.length; i++) {
-        if (ages[i] > ages[i + 1]) { 
-            oldest = ages[i];
-        }
-    }
-    return _.filter(array, function(customerObj) {
-        if (customerObj.age === oldest) {
-            return true; 
-        }
-    })[0].name; 
+var oldestCustomer = function(array) { //func takes array of customer objects
+    return _.reduce(array, function(prev, current) {//reduce to cycle thru cust's
+        if (current.age > prev.age) { //if current cust's age is older than last:
+            prev = current; //assign current cust to prev to keep checking against
+        }  
+        return prev;        //return prev when done checking all ages
+    }).name; //instead of returning entire customer object, access only the name
 }; 
 
 
 
-var youngestCustomer = function(array) {
-    var ages = _.pluck(array, 'age'); 
-    var youngest = 0; 
-    for (let i = 0; i < ages.length; i++) {
-        if (ages[i] < ages[i - 1]) {
-            youngest = ages[i];
+var youngestCustomer = function(array) { //func takes array of customer objects
+    return _.reduce(array, function(prev, current){//reduce to cycle thru cust's
+        if (current.age < prev.age) { //if current cust's age is less than last:
+            prev = current; //assign current cust to prev to keep checking against
         }
-    }
-    return _.filter(array, function(customerObj) {
-        if (customerObj.age === youngest) {
-            return true;
-        }
-    })[0].name;
+        return prev; //return prev when done checking all ages
+    }).name; //instead of returning entire customer object, access only the name
 };
 
 
 
-var averageBalance = function(array) {
-    var balances = _.pluck(array, 'balance');
-    var numBal = []; 
-    _.each(balances, function(element, key, index) {
-        numBal.push(parseFloat(element.replace(/[^0-9.-]+/g,"")));
+var averageBalance = function(array) { //func takes array of customer objects
+    var balances = _.pluck(array, 'balance');   //pluck balances to new array
+    var numBal = _.map(balances, function(balAmt, key, index) {//map strings...
+        return parseFloat(balAmt.replace(/[^0-9.-]+/g,""));//of balances to #'s'
         });
-    var totalBal = 0; 
-    for (var i = 0; i < numBal.length; i++) { //*****CHANGE TO FUNC
-        totalBal += numBal[i];
-    } 
-    /////////////   console.log(totalBal); 
-    return totalBal/numBal.length;
+    return _.reduce(numBal, function(prev, current) {//reduce to add all bal's 
+        prev += current;    //add each current balance element to the previous
+        return prev;        //return previous (the total)
+    })/numBal.length;       //divide total of all balances by number of balances
 }; 
 
-var firstLetterCount;
 
-var friendFirstLetterCount;
 
-var friendsCount;
+var firstLetterCount = function(array, letter) {//takes cust arr and match letter
+    var fullNames = _.pluck(array, 'name'); //pluck cust names to new array
+    return _.reduce(fullNames, function(prev, current) {//reduce to check names
+        //if name's 1st letter (lower cased) matches input letter (lower cased):
+        if (current[0].toLowerCase() === letter.toLowerCase()) {
+            prev += 1;                      // increment the count
+        }
+    return prev;                            // return prev (the count)
+    }, 0);                                  // seed of 0 to start counting from 
+};
 
-var topThreeTags;
 
-var genderCount;
+                        //takes cust arr, specific customer, and friend to find
+var friendFirstLetterCount = function(array, customer, letter) {
+    var selectCust = _.filter(array, function(customerObjs) {//find cust w/ filter
+       return customerObjs.name === customer; //return entire cust object (array)
+    });
+    var friendsList = _.pluck(selectCust, 'friends')[0];//pluck friends from arr
+    return _.reduce(friendsList, function(prev, current) {//reduce to count frnds
+    //if name's first letter (lower cased) matches input letter (lower cased):
+       if (current.name[0].toLowerCase() === letter.toLowerCase()) {
+           prev += 1;                       // increment the count
+       }
+    return prev;                            // return prev (the count)
+    }, 0);                                  // seed of 0 to start counting from 
+};
+
+
+
+var friendsCount = function(array, name) { // takes cust arr and friend name
+    var custNames = []; // empty array to push customer name result to 
+    _.each(array, function(cust, i, custList) { // each to check ever customer
+        // using some, if any of the customer's friends match the input name:
+        if (_.some(cust.friends, function(friend, index, friendsList) {
+                return (friend.name === name);
+        })) {
+            custNames.push(cust.name);  // push customer's name to result array
+        }
+    }); 
+    return custNames;                   // return result array of customer names
+};
+    
+
+
+var topThreeTags = function(array) { // function takes array of customer objects
+    var flattenedTags = []; // empty array for all tags (flatten indiv. arrays)
+    _.each(array, function(cust, i, custList) { //use each for each customer obj
+        _.each(cust.tags, function(tag, index, tagList) {//each again for tag arr
+            flattenedTags.push(tag);            //push all tags to empty array
+        });
+    }); 
+    // reduce to count all tags in array of tags
+    var tagCountObj = _.reduce(flattenedTags, function(countObj, tag) {
+        if (countObj[tag]) {        // if count object already contains tag: 
+            countObj[tag]++;        // increment the tag's count by 1
+        } else {                    // otherwise (count obj doesn't have tag):
+            countObj[tag] = 1;      // add tag to count object with a count of 1
+        }
+        return countObj;            // return object when counting is complete
+    }, {}); 
+    // use map to make new array with each tag and its count (see below)
+    var tagCountArr = _.map(tagCountObj, function(count, tag, obj) {         
+        return [tag, count]; // each element of new array will have nested array
+    }); 
+    tagCountArr.sort(function(a, b) {   // sort tag count array 
+        return b[1] - a[1]; // sort in reverse (b-a) by the count (position 1)
+    });
+    var top3 = tagCountArr.slice(0, 3); // slice count array down to top 3 only
+    return _.map(top3, function(resultArray) {  // use map to return array...
+        return resultArray[0];                  // containing only the tag words
+    });
+};
+
+
+
+var genderCount = function(array) { // function takes array of customer objects
+    var genderArr = _.pluck(array, 'gender'); // pluck genders to new array
+    return _.reduce(genderArr, function(countObj, gender) {//reduce: count genders
+        if (countObj[gender]) {     // if count obj already contains gender:
+            countObj[gender]++;     // increment the gender's count by 1
+        } else {                    // otherwise (count obj doesn't have gender):
+            countObj[gender] = 1;   // add gender to count obj with a count of 1
+        }
+        return countObj;            // return object when count is complete
+    }, {}); 
+};
+
+
+
 
 //////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE ////////////////////////////////////////////
